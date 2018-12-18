a57c8504576c37f49eb61ceaf5d6135bd4ff8ea8
Lucas Dahl <LucasDahl425@yahoo.com>
2018-12-18 07:17:30 -0800

    Started to set up the DescriptionViewController class, and hooked up more outlets.

--- CollectionViewSegueModel/DescriptionViewController.swift
+++ CollectionViewSegueModel/DescriptionViewController.swift
@@ -8,18 +8,90 @@
 
 import UIKit
 
+protocol DescriptionViewControllerprotocol {
+    
+    func descriptionViewDismissed()
+    
+}
+
 class DescriptionViewController: UIViewController {
     
     // Outlets
     @IBOutlet weak var dimView: UIView!
+    @IBOutlet weak var descriptionText: UITextView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var dialogView: UIView!
     
+    // Properties
+    var delegate:DescriptionViewControllerprotocol?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
+        
     }
     
-
+    override func viewWillAppear(_ animated: Bool) {
+        
+        // Set the rounded corners for the dialogView
+        dialogView.layer.cornerRadius = 15
+        
+        // Set the alpha
+        dimView.alpha = 0
+        titleLabel.alpha = 0
+        descriptionText.alpha = 0
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        // Sets the dim view alpha back to one, by animating it in.
+        UIView.animate(withDuration: 1, delay: 0, options: .curveEaseOut, animations: {
+            
+            self.dimView.alpha = 1
+            
+        }, completion: nil)
+        
+    }
+    
+    func setPopup(withTitle:String, withMessage:String) {
+        
+        titleLabel.text = withTitle
+        descriptionText.text = withMessage
+        
+        // Fade in the labels
+        UIView.animate(withDuration: 0.5, delay: 0, options: .curveEaseOut, animations: {
+            
+            self.titleLabel.alpha = 1
+            self.descriptionText.alpha = 1
+            
+        }, completion: nil)
+        
+    }
+    
+    // Dismiss button
+    @IBAction func dismissTapped(_ sender: UIButton) {
+        
+        UIView.animate(withDuration: 0.3, delay: 0, options: .curveEaseOut, animations: {
+            
+            self.dimView.alpha = 0
+            
+        }) { (completed) in
+            
+            // Only to dismiss after dim view has faded out.
+            self.dismiss(animated: true, completion: {
+                
+                self.titleLabel.text = ""
+                self.descriptionText.text = ""
+                
+            })
+            
+            self.delegate?.descriptionViewDismissed()
+            
+        }
+        
+    }
+    
  
 }
